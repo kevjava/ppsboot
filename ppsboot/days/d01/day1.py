6d1926e04713d5228fca2503fc123718f49f78f0
Kevin Wallace <kev@kevinja.com>
2023-11-30 19:57:08 -0600

    Documentation.

--- ppsboot/days/d01/day1.py
+++ ppsboot/days/d01/day1.py
@@ -15,7 +15,7 @@ class Day1(Solution):
             filename (str): The name of the input file.
         
         Returns:
-            list: The input file as a list of strings.
+            list: The input file as a list of ints.
         """
         return InputFile(filename).as_single_ints()
         
@@ -34,8 +34,17 @@ class Day1(Solution):
     
     def sliding_window(self, iterable, n):
         """ Stolen mercilessly from 
-        [here](https://docs.python.org/3/library/itertools.html#itertools-recipes) """
-        # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+        [here](https://docs.python.org/3/library/itertools.html#itertools-recipes) 
+
+        `sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG`
+
+        Args:
+            iterable (iterable): The iterable to slide over.
+            n (int): The size of the window.
+
+        Yields:
+            tuple: The next window.
+        """
         it = iter(iterable)
         window = collections.deque(itertools.islice(it, n-1), maxlen=n)
         for x in it:
